c8974b7efe93bb7ffd621766da12b3eb6b7fedfe
ghali-faten <fatenghali@hotmail.fr>
2022-12-19 21:55:03 +0100

    faten

---
+++ main.js
@@ -0,0 +1,35 @@
+var img = document.querySelector(".img");
+var container =document.querySelector(".container");
+function aloes(drink){
+	img.src = drink;
+}
+function colors(color){
+	container.style.background = color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
